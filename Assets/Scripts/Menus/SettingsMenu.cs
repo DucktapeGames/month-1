f7f5e08e7621af1c2716ae53398a60027a858702
julio177 <julioc.aguilarv@gmail.com>
2017-06-15 22:46:21 -0500

    Recycles settings scene from week 1 game

---
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour {
+
+	public Slider VolumeSlider;
+	public Settings SettingsRef;
+
+	// Use this for initialization
+	void Start () {
+		VolumeSlider.value = SettingsRef.Volume;
+	}
+
+	public void GoToMenu () {
+		SceneManager.LoadScene(0);
+	}
+}
