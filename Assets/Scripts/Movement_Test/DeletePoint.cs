52e261a4b9f92522e39269793019a6ee71a70736
batomow <carlosa_miranda@outlook.com>
2017-06-16 18:36:54 -0700

    finished some bootleg starfox

---
+++ Assets/Scripts/Movement_Test/DeletePoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeletePoint : MonoBehaviour {
+
+	public Transform RespawnPoint; 
+
+
+	void OnTriggerEnter(Collider something){
+		Debug.Log ("Hey"); 
+		something.transform.position = new Vector3 (something.transform.position.x,something.transform.position.y, RespawnPoint.position.z); 
+	}
+}
