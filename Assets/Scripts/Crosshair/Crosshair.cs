b0c9f106bad0b73826414b8e4285bb2b905194c0
batomow <carlosa_miranda@outlook.com>
2017-06-20 00:47:21 -0700

    finished the crosshair

--- Assets/Scripts/Crosshair/Crosshair.cs
+++ Assets/Scripts/Crosshair/Crosshair.cs
@@ -2,24 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Crosshair2 : MonoBehaviour {
+public class Crosshair : MonoBehaviour {
 
 	private Transform player; 
-	public float depth; 
+	public float ScreenDepth; 
+	public float Depth; 
 	private Vector3 direction; 
 	private Vector3 screenPos; 
+	private Vector3 targetPos; 
 
 	void Awake(){
 		player = GameObject.FindGameObjectWithTag ("Player").transform; 
 	}
 
 	void Update(){
-		screenPos = Camera.main.ScreenPointToRay (Input.mousePosition).GetPoint (depth);
+		screenPos = Camera.main.ScreenPointToRay (Input.mousePosition).GetPoint (ScreenDepth);
 		direction = screenPos - player.position; 
+		targetPos = player.position + direction.normalized * Depth; 
 	}
+
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		this.transform.position = player.position + direction.normalized * depth;
+		this.transform.position = targetPos;  
 	}
 }
