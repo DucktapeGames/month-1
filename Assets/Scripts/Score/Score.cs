003b0763f36622cf20fa8f3213cc6f7dac125f99
julio177 <julioc.aguilarv@gmail.com>
2017-07-04 13:58:34 -0500

    Crea script para manejar el score con funcion publica AddScore(int)

---
+++ Assets/Scripts/Score/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+	public GUIText scoreText;
+	private int score;
+	void Start () {
+		score = 0;
+		UpdateScore();
+	}
+	
+	public void AddScore(int value) {
+		score += value;
+		UpdateScore();
+	}
+
+	void UpdateScore() {
+		scoreText.text = "Score: " + score;
+	}
+}
